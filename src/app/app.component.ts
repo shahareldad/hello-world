77b3a998f3128178ca5a5dd3833642848b582395
Shahar Eldad <shahar.e@gmail.com>
2025-01-02 21:23:55 +0200

    Continued learning

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,19 +12,33 @@ import { FormsModule } from '@angular/forms';
 })
 export class AppComponent {
   items: WishItem[] = [
-    // new WishItem('To run faster'),
-    // new WishItem('To have some coffee', true),
-    // new WishItem('To learn Angular')
+    new WishItem('To run faster'),
+    new WishItem('To have some coffee', true),
+    new WishItem('To learn Angular')
   ];
+  get visibleItems(): WishItem[] {
+    if (this.selectedFilter === '0') return this.items;
+    else if (this.selectedFilter === '1') return this.items.filter((item) => !item.isComplete);
+    else if (this.selectedFilter === '2') return this.items.filter((item) => item.isComplete);
+    else return [];
+  }
+
+  selectedFilter: string = '0';
   title = 'hello-world';
   newWishText: string = '';
 
+  // filterWishes(value: any) {
+  //   if (value === '0') this.visibleItems = this.items;
+  //   else if (value === '1') this.visibleItems = this.items.filter((item) => !item.isComplete);
+  //   else if (value === '2') this.visibleItems = this.items.filter((item) => item.isComplete);
+  // }
   toggleWishItem(item: any) {
     item.isComplete = !item.isComplete;
-    // console.dir(item);
+    // this.filterWishes(this.selectedFilter);
   }
   addNewWish() {
     this.items.push(new WishItem(this.newWishText));
     this.newWishText = '';
+    // this.filterWishes(this.selectedFilter);
   }
 }
