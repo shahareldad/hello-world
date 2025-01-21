7ca83f151430fe8f3d069b9b3bef7dc9a14b091c
shahareldad <shahar.e@gmail.com>
2025-01-21 16:44:50 +0200

    Continued learning

--- src/app/wish-repo.service.ts
+++ src/app/wish-repo.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { WishItem } from '../shared/modules/wishitem';
 
 @Injectable({
   providedIn: 'root'
@@ -8,10 +9,29 @@ import { Observable } from 'rxjs';
 export class WishRepoService {
   private wishesUrl = 'wishes_repo.json';
 
+  private getStandardOptions() : any {
+    return {
+      headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+    }
+  };
+
   constructor(private http: HttpClient) { }
 
   getWishes(): Observable<any> {
-    console.dir(['WishRepoService','getWishes', 'started']);
-    return this.http.get<any>(this.wishesUrl);
+    // console.dir(['WishRepoService','getWishes', 'started']);
+    let options = this.getStandardOptions();
+    debugger;
+    options.params = new HttpParams({
+      fromObject: {
+        format: 'json'
+      }
+    });
+    return this.http.get<any>(this.wishesUrl, options);
+  }
+
+  private addWish(wish: WishItem) {
+    let options = this.getStandardOptions();
+    options.headers = options.headers.set('authorization', 'some_value');
+    return this.http.post<any>(this.wishesUrl, wish, options);
   }
 }
