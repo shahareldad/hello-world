d0025d86a42241e866a72065671b0c974869c6e7
shahareldad <shahar.e@gmail.com>
2025-02-03 16:22:31 +0200

    Continued learning

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { WishComponent } from './wish/wish.component';
+import { ContactComponent } from './contact/contact.component'; 
 
 @Component({
   selector: 'app-root',
-  imports: [ WishComponent ],
+  imports: [ WishComponent, ContactComponent ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,11 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, importProvidersFrom } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-contact',
-  imports: [],
+  imports: [ ReactiveFormsModule ],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.css'
 })
 export class ContactComponent {
+
+  senderNameControl = new FormControl('');
+  senderEmailControl = new FormControl('');
+  senderMessageControl = new FormControl('');
+  submitForm() {
+    if (this.senderNameControl.dirty) {
+      alert('Name field has changed');
+    }
+  }
 
 }
